90e12ad4005638bc8030735c5de811a4a8802305
8times12 <8times12@gmail.com>
2020-07-26 22:28:45 +0900

    TypeScript-React-Starter: create store
    ストア作成完了。コンポーネントをコンテナに置換。動作確認はまだだが、ガイドの内容は一通り完了。

--- TypeScript-React-Starter/my-app/src/index.tsx
+++ TypeScript-React-Starter/my-app/src/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './components/Hello';
 import * as serviceWorker from './serviceWorker';
+import { StoreState } from './types';
+import { createStore, Store } from 'redux';
+import { enthusiasm } from './reducers';
+import { EnthusiasmAction } from './actions';
+import { Provider } from 'react-redux';
+import Hello from './containers/Hello';
+
+const store: Store<StoreState, EnthusiasmAction> = createStore(enthusiasm, {
+    enthusiasmLevel: 1,
+    languageName: 'TypeScript'
+});
 
 ReactDOM.render(
-  <Hello name="TypeScript" enthusiasmLevel={5} />,
-  document.getElementById('root') as HTMLElement
+    <Provider store={store}>
+        <Hello />
+    </Provider>,
+    document.getElementById('root') as HTMLElement
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
--- TypeScript-React-Starter/my-app/src/reducers/index.tsx
+++ TypeScript-React-Starter/my-app/src/reducers/index.tsx
@@ -1,8 +1,12 @@
 import { EnthusiasmAction } from "../actions";
 import { StoreState } from "../types";
 import { INCREMENT_ENTHUSIASM, DECREMENT_ENTHUSIASM } from "../constants";
+import { Reducer } from "redux";
 
-export function enthusiasm(state: StoreState, action: EnthusiasmAction): StoreState {
+export const enthusiasm: Reducer<StoreState, EnthusiasmAction> = (state, action) => {
+    if (typeof state === 'undefined') {
+        throw new Error('State is undefined.');
+    }
     switch (action.type) {
     case INCREMENT_ENTHUSIASM:
         return {
