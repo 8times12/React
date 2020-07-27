dc732071cab783d84882168228ab0428c700dfeb
8times12 <8times12@gmail.com>
2020-07-27 22:57:14 +0900

    Tic Tac Toe: base

---
+++ react-tutorial/src/components/Square.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Square.scss';
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button type="button" className="square">
+                {/* TODO */}
+            </button>
+        );
+    }
+}
+
+export default Square;
---
+++ react-tutorial/src/index.tsx
@@ -0,0 +1,9 @@
+import Game from "./components/Game";
+import ReactDOM from "react-dom";
+import React from "react";
+import './index.scss';
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
